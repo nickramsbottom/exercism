b6eac6991f1df9b7568fc37d0201cdb6981eaf87
Nick Ramsbottom <hi@nickramsbottom.com>
2020-03-09 11:13:26 +0000

    Add solution to Go parallel letter frequency

--- go/parallel-letter-frequency/parallel_letter_frequency.go
+++ go/parallel-letter-frequency/parallel_letter_frequency.go
@@ -12,3 +12,24 @@ func Frequency(s string) FreqMap {
 	}
 	return m
 }
+
+// ConcurrentFrequency finds the frequency of letters in multiple input texts
+func ConcurrentFrequency(texts []string) FreqMap {
+	results := FreqMap{}
+	resultChannel := make(chan FreqMap, 10)
+
+	for _, text := range texts {
+		go func(s string) {
+			resultChannel <- Frequency(s)
+		}(text)
+	}
+
+	for range texts {
+		result := <-resultChannel
+		for r, count := range result {
+			results[r] += count
+		}
+	}
+
+	return results
+}
