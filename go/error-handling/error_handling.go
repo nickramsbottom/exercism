f9741ed86e765e3b5b2bd242b1c1e72dc5b895a3
Nick Ramsbottom <hi@nickramsbottom.com>
2020-04-03 18:06:51 +0100

    Refactor... refactor a lot

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,34 +1,28 @@
 package erratum
 
 // Use an input with a resource from the opener
-func Use(o ResourceOpener, input string) (e error) {
-	var err error
-	var res Resource
+func Use(o ResourceOpener, input string) (result error) {
+	resource, err := o()
+	for err != nil {
+		if _, ok := err.(TransientError); !ok {
+			return err
+		}
+		resource, err = o()
+	}
+
+	defer func() {
+		resource.Close()
+	}()
 
 	defer func() {
 		if r := recover(); r != nil {
-			frobError, isFrobError := r.(FrobError)
-
-			if isFrobError {
-				res.Defrob(frobError.defrobTag)
+			if frob, ok := r.(FrobError); ok {
+				resource.Defrob(frob.defrobTag)
 			}
-
-			e = r.(error)
-			res.Close()
+			result = r.(error)
 		}
 	}()
 
-	res, err = o()
-
-	for err != nil && err.Error() == "some error" {
-		res, err = o()
-	}
-
-	if err != nil && err.Error() != "some error" {
-		return err
-	}
-
-	res.Frob(input)
-	res.Close()
-	return err
+	resource.Frob(input)
+	return nil
 }
