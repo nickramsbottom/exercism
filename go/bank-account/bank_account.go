38432eb153bb0816ac703b437b1eafd0fe73f814
Nick Ramsbottom <hi@nickramsbottom.com>
2020-04-04 12:23:04 +0100

    Sequential solution

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -1,1 +1,48 @@
 package account
+
+// Account at the bank
+type Account struct {
+	balance int64
+	open    bool
+}
+
+// Open an account
+func Open(initialDeposit int64) *Account {
+	if initialDeposit < 0 {
+		return nil
+	}
+
+	return &Account{
+		balance: initialDeposit,
+		open:    true,
+	}
+}
+
+// Close the account
+func (a *Account) Close() (payout int64, ok bool) {
+	a.open = false
+	return a.balance, true
+}
+
+// Balance of the account
+func (a Account) Balance() (balance int64, ok bool) {
+	if !a.open {
+		return 0, false
+	}
+
+	return a.balance, true
+}
+
+// Deposit change account balance
+func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
+	if !a.open {
+		return 0, false
+	}
+
+	if amount+a.balance < 0 {
+		return a.balance, false
+	}
+
+	a.balance = amount + a.balance
+	return a.balance, true
+}
