d54ad18c983eac3b866757e9a41a612a19e2b233
Nick Ramsbottom <hi@nickramsbottom.com>
2020-05-05 06:58:17 +0100

    Compare length of rune slices instead of strings

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -6,19 +6,16 @@ var errDiffLength = errors.New("strings must be of equal length")
 
 // Distance returns the hamming distance between two DNA strings
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
+	ar, br := []rune(a), []rune(b)
+	if len(ar) != len(br) {
 		return 0, errDiffLength
 	}
 
 	hamming := 0
-
-	ar, br := []rune(a), []rune(b)
-
 	for i := range ar {
 		if ar[i] != br[i] {
 			hamming++
 		}
 	}
-
 	return hamming, nil
 }
