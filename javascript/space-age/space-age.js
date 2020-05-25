1b4769b6cc5a4d78a1ba4eb6e6d6f39456112abc
Nick Ramsbottom <hi@nickramsbottom.com>
2020-05-25 21:46:00 +0100

    Implement space age

---
+++ javascript/space-age/space-age.js
@@ -0,0 +1,15 @@
+const relativePeriods = {
+	mercury: 0.2408467,
+	venus: 0.61519726,
+	earth: 1.0,
+	mars: 1.8808158,
+	jupiter: 11.862615,
+	saturn: 29.447498,
+	uranus: 84.016846,
+	neptune: 164.79132,
+}
+
+const earthYearMs = 31557600;
+
+export const age = (planet, timeMs) =>
+	Number((timeMs/(earthYearMs * relativePeriods[planet])).toFixed(2));
