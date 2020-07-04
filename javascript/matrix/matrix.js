6ef41daa239117010b1b8f8452b4678972e25385
Nick Ramsbottom <hi@nickramsbottom.com>
2020-07-04 14:14:10 +0100

    Refactor matrix JavaScript
    Returning `this.rows` returns by reference. You need to send a copy
    to make sure that consumers don't modify internal variables.

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,26 +1,38 @@
 export class Matrix {
-	constructor(string) {
-		this.matrix = string.split('\n').map(row => row.split(' ').map(Number))
+	constructor(matrixRows) {
+		this._matrix = this.parse(matrixRows);
 	}
 
 	get rows() {
-		return this.matrix;
+		return copy(this._matrix);
 	}
 
 	get columns() {
-		const height = this.matrix.length;
-		const width = this.matrix[0].length;
-
-		const transposed = [];
-
-		for (let i = 0; i < width; i++) {
-			const col = [];
-			for (let j = 0; j < height; j++) {
-				col.push(this.matrix[j][i]);
-			}
-			transposed.push(col);
-		}
-
-		return transposed;
+		return transpose(this._matrix);
 	}
+
+	parse(string) {
+		return string.split('\n').map(row => row.split(' ').map(Number))
+	}
+}
+
+function copy(matrix) {
+	return matrix.map(row => row.map(number => number));
+}
+
+function transpose(matrix) {
+	const height = matrix.length;
+	const width = matrix[0].length;
+
+	const transposed = [];
+
+	for (let i = 0; i < width; i++) {
+		const col = [];
+		for (let j = 0; j < height; j++) {
+			col.push(matrix[j][i]);
+		}
+		transposed.push(col);
+	}
+
+	return transposed;
 }
