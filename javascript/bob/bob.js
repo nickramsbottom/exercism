728f37d9f4e629d46eb79105e5f6439d3ed45087
Nick Ramsbottom <hi@nickramsbottom.com>
2020-09-27 11:49:31 +0100

    Initial JavaScript Bob solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,8 +1,24 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const isQuestion = message => message.endsWith('?');
+const isShouting = message => message === message.toUpperCase() && (/[a-zA-Z]/.test(message));
 
-export const hey = (message) => {
-  throw new Error("Remove this statement and implement this function");
+export const hey = message => {
+	const trimmed = message.trim();
+
+	if (trimmed == '') {
+		return 'Fine. Be that way!';
+	}
+
+	if (isShouting(trimmed) && isQuestion(trimmed)) {
+		return 'Calm down, I know what I\'m doing!';
+	}
+
+	if (isShouting(trimmed)) {
+		return 'Whoa, chill out!';
+	}
+
+	if (isQuestion(trimmed)) {
+		return 'Sure.';
+	}
+
+	return 'Whatever.';
 };
