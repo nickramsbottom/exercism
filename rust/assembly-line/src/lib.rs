6236692e4b51466fea1732f498984e0ceb136ee3
Nick Ramsbottom <hi@nickramsbottom.com>
2021-10-08 17:33:42 +0100

    Use match to assign

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,11 +1,12 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     let speed_rate: f64 = 221.0 * speed as f64;
-    match speed {
+    let ok_rate = match speed {
         0 => 0.0,
-        1..=4 => speed_rate,
-        5..=8 => speed_rate * 0.9,
-        _ => speed_rate * 0.77,
-    }
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        _ => 0.77,
+    };
+    speed_rate * ok_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
