01ae06474f9e7d18b87231aab796d4fc3ae5bf00
Nick Ramsbottom <hi@nickramsbottom.com>
2021-10-08 08:12:56 +0100

    Use constant for production rate

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,14 +1,16 @@
 #![allow(unused)]
 
+const PRODUCTION_RATE_DEFAULT: f64 = 221.0;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     if speed == 0 {
         return 0.0;
     } else if speed <= 4 {
-        return 221.0 * speed as f64;
+        return PRODUCTION_RATE_DEFAULT * speed as f64;
     } else if speed <= 8 {
-        return 0.9 * 221.0 * speed as f64;
+        return PRODUCTION_RATE_DEFAULT * 0.9 * speed as f64;
     } else {
-        return 0.77 * 221.0 * speed as f64;
+        return PRODUCTION_RATE_DEFAULT * 0.77 * speed as f64;
     }
 }
 
