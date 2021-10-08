e983410f149e794f7917b77a46c3271a70359275
Nick Ramsbottom <hi@nickramsbottom.com>
2021-10-08 17:32:01 +0100

    Refactor use of variables

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,12 +1,10 @@
-const PRODUCTION_RATE_DEFAULT: f64 = 221.0;
-
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    let s = speed as f64;
+    let speed_rate: f64 = 221.0 * speed as f64;
     match speed {
         0 => 0.0,
-        1..=4 => PRODUCTION_RATE_DEFAULT * s,
-        5..=8 => PRODUCTION_RATE_DEFAULT * 0.9 * s,
-        _ => PRODUCTION_RATE_DEFAULT * 0.77 * s,
+        1..=4 => speed_rate,
+        5..=8 => speed_rate * 0.9,
+        _ => speed_rate * 0.77,
     }
 }
 
