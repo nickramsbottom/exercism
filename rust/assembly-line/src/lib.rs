cee4ac6e926de92c03de6ce281f7ea8ec4ad5baa
Nick Ramsbottom <hi@nickramsbottom.com>
2021-10-08 14:26:08 +0100

    Move speed casting to temporary variable

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,11 +1,12 @@
 const PRODUCTION_RATE_DEFAULT: f64 = 221.0;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let s = speed as f64;
     match speed {
         0 => 0.0,
-        1..=4 => PRODUCTION_RATE_DEFAULT * speed as f64,
-        5..=8 => PRODUCTION_RATE_DEFAULT * 0.9 * speed as f64,
-        _ => PRODUCTION_RATE_DEFAULT * 0.77 * speed as f64,
+        1..=4 => PRODUCTION_RATE_DEFAULT * s,
+        5..=8 => PRODUCTION_RATE_DEFAULT * 0.9 * s,
+        _ => PRODUCTION_RATE_DEFAULT * 0.77 * s,
     }
 }
 
