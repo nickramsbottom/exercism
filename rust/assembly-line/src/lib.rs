b20261bebc4861bc56caed6cc4f6a8bf7a8081b1
Nick Ramsbottom <hi@nickramsbottom.com>
2021-10-08 14:23:11 +0100

    Refactor conditional to matcher

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,14 +3,11 @@
 const PRODUCTION_RATE_DEFAULT: f64 = 221.0;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    if speed == 0 {
-        return 0.0;
-    } else if speed <= 4 {
-        return PRODUCTION_RATE_DEFAULT * speed as f64;
-    } else if speed <= 8 {
-        return PRODUCTION_RATE_DEFAULT * 0.9 * speed as f64;
-    } else {
-        return PRODUCTION_RATE_DEFAULT * 0.77 * speed as f64;
+    match speed {
+        0 => 0.0,
+        1..=4 => PRODUCTION_RATE_DEFAULT * speed as f64,
+        5..=8 => PRODUCTION_RATE_DEFAULT * 0.9 * speed as f64,
+        _ => PRODUCTION_RATE_DEFAULT * 0.77 * speed as f64,
     }
 }
 
