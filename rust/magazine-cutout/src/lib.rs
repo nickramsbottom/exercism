c98cff2318b351c9588db4f0e536179397442ccf
Nick Ramsbottom <hi@nickramsbottom.com>
2021-10-26 20:36:25 +0100

    Refactor populating hashmap

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -4,7 +4,10 @@ pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
     let mut available_words = HashMap::new();
 
     for word in magazine {
-        *available_words.entry(word).or_insert(0) += 1;
+        available_words
+            .entry(word)
+            .and_modify(|value| *value += 1)
+            .or_insert(1);
     }
 
     for word in note {
