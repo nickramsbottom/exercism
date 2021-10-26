5c2a8b139a2f565617d1fb52e2ecf48bbf253b07
Nick Ramsbottom <hi@nickramsbottom.com>
2021-10-26 19:56:10 +0100

    Rust rpn calculator first iteration

---
+++ rust/rpn-calculator/src/lib.rs
@@ -0,0 +1,46 @@
+#[derive(Debug)]
+pub enum CalculatorInput {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Value(i32),
+}
+
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    let mut vec: Vec<i32> = Vec::new();
+
+    // there's a lot of repetition in here, can we group
+    // everything except value into a closure?
+    for input in inputs {
+        match input {
+            CalculatorInput::Add => {
+                let first = vec.pop()?;
+                let second = vec.pop()?;
+                vec.push(first + second);
+            }
+            CalculatorInput::Subtract => {
+                let first = vec.pop()?;
+                let second = vec.pop()?;
+                vec.push(second - first);
+            }
+            CalculatorInput::Multiply => {
+                let first = vec.pop()?;
+                let second = vec.pop()?;
+                vec.push(first * second);
+            }
+            CalculatorInput::Divide => {
+                let first = vec.pop()?;
+                let second = vec.pop()?;
+                vec.push(second / first);
+            }
+            CalculatorInput::Value(val) => vec.push(*val),
+        }
+    }
+
+    if vec.len() != 1 {
+        return None;
+    }
+
+    vec.pop()
+}
